a86b8edced3c40dac5dc8c8579c6294b8cc4e113
awrion3 <121172065+awrion3@users.noreply.github.com>
2022-12-26 19:44:19 +0900

    change recent file

--- Hello.py
+++ Hello.py
@@ -1,3 +1,11 @@
+#Print Only One Sentence.
 print("Hello World!")
+#Print Line Break.
+print()
+#Print Keyword List, and Line Break.
 import keyword
 print(keyword.kwlist)
+print()
+#Print Multiple Words.
+print("I have", 2, "flowers.")
+print("Goodbye!"*3)
